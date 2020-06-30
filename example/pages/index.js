925606fb9ad86047c099cb150037dc6b26ab3773
Faraz Khan <farazrk001@gmail.com>
2020-06-30 17:44:57 +0530

    Initial commit

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,24 +1,20 @@
-import { useImagesLoaded } from '../../use-images-loaded';
+import { useImagesLoaded } from '../../use-images-loaded'
 
-import { toCamel } from '../lib/util';
+import { toCamel } from '../lib/util'
 
-import hookConfig from '../../use-images-loaded/package.json';
+import hookConfig from '../../use-images-loaded/package.json'
 
 export default function Index() {
-  const { name, description, repository = {}, author = {} } = hookConfig;
+  const { name, description, repository = {}, author = {} } = hookConfig
 
-  const { name: authorName, url: authorUrl } = author;
+  const { name: authorName, url: authorUrl } = author
 
-  const { url: repositoryUrl } = repository;
-  const repositoryExists = typeof repositoryUrl === 'string';
+  const { url: repositoryUrl } = repository
+  const repositoryExists = typeof repositoryUrl === 'string'
 
-  const repositoryUrlDisplay = repositoryExists && repositoryUrl.split('://')[1];
+  const repositoryUrlDisplay = repositoryExists && repositoryUrl.split('://')[1]
 
-  const hookSettings = {
-    message: 'Hello, custom hook!'
-  }
-
-  const { message } = useImagesLoaded(hookSettings);
+  const [ref, loaded] = useImagesLoaded()
 
   return (
     <main>
@@ -33,20 +29,28 @@ export default function Index() {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
-          padding: 1em 0;
+          padding: 1em 1em;
         }
 
         h1 {
           font-size: 2em;
         }
 
+        .img-container {
+          display: grid;
+          grid-template-columns: auto auto auto;
+          justify-items: start;
+          grid-gap: 0.25rem;
+        }
+
         img {
           max-width: 100%;
+          padding: 0.25em;
         }
 
         pre {
           overflow: auto;
-          max-height: 15em;
+          max-height: 18em;
           background-color: #eeeeee;
           padding: 1em;
         }
@@ -59,7 +63,7 @@ export default function Index() {
         }
 
         footer p {
-          font-size: .9em;
+          font-size: 0.9em;
         }
 
         footer p,
@@ -69,54 +73,61 @@ export default function Index() {
       `}</style>
 
       <section>
-
-        <h1>{ toCamel(name) }</h1>
-
-        <p>{ description }</p>
-
-        { repositoryExists && (
+        <h1>{toCamel(name)}</h1>
+        <p>{description}</p>
+        {repositoryExists && (
           <p>
-            <a href={repositoryUrl}>
-              { repositoryUrlDisplay }
-            </a>
+            <a href={repositoryUrl}>{repositoryUrlDisplay}</a>
           </p>
         )}
-
-        <h2>How to use</h2>
-
-        <p>
-          Add your instructions here!
-        </p>
-
-        <h2>Examples</h2>
-
-        <h3>Set and grab message</h3>
-        <p>
-          <strong>Input:</strong>
-        </p>
+        <h2>Installation</h2>
+        <p>Install with npm</p>
+        <pre>
+          <code>npm install use-images-loaded</code>
+        </pre>
+        <p>Install with yarn</p>
+        <pre>
+          <code>yard add use-images-loaded</code>
+        </pre>
+        <h2>Usage</h2>
+        <p>Displaying a loading indicator while images are loading in a container</p>
         <pre>
           <code>
-{`const hookSettings = {
-  message: 'Hello, custom hook!'
-}
+            {`import useImageLoaded from 'use-image-loaded'
 
-const { message } = useImagesLoaded(hookSettings);`}
+const ImageContainer = () => {
+  const [ref, loaded] = useImagesLoaded()
+
+  return (
+    <div ref={ref}>
+    <p> Status: {loaded ? 'Loaded': 'Loading'} </p>
+    <img src="https://unsplash.it/200/200" alt="image"/>
+    <img src="https://unsplash.it/200/200" alt="image"/>
+    <img src="https://unsplash.it/200/200" alt="image"/>
+    </div>
+  )
+}
+`}
           </code>
         </pre>
-        <p>
-          <strong>Output:</strong>
-        </p>
-        <p>
-          { message }
-        </p>
+
+        <h2>See it in action</h2>
+
+        <div ref={ref}>
+          <p> Status: {loaded ? 'Loaded' : 'Loading'} </p>
+          <div className="img-container">
+            <img src="https://unsplash.it/2000/2000" alt="image" />
+            <img src="https://unsplash.it/2000/2000" alt="image" />
+            <img src="https://unsplash.it/2000/2000" alt="image" />
+          </div>
+        </div>
       </section>
 
       <footer>
         <p>
-          Made by <a href={authorUrl}>{ authorName }</a>
+          Made by <a href={authorUrl}>{authorName}</a>
         </p>
       </footer>
     </main>
-  );
-
+  )
 }
--- use-images-loaded/src/useImagesLoaded.js
+++ use-images-loaded/src/useImagesLoaded.js
@@ -1,6 +1,31 @@
-export default function useImagesLoaded (settings = {}) {
-  const { message } = settings;
-  return {
-    message
-  };
+'use strict'
+import { useEffect, useState } from 'react'
+
+const useImagesLoaded = () => {
+  const [ref, setRef] = useState(null)
+  const [imagesLoaded, setImagesLoaded] = useState(false)
+
+  useEffect(() => {
+    let promisesArray = []
+    if (!ref) return
+    const imageElements = ref.querySelectorAll('img')
+    imageElements.forEach((img) => {
+      if (!img.complete) {
+        let promise = new Promise((resolve) => {
+          img.addEventListener('load', () => {
+            resolve()
+          })
+        })
+        promisesArray.push(promise)
+      }
+    })
+    if (promisesArray.length > 0) {
+      Promise.all(promisesArray).then(() => {
+        setImagesLoaded(true)
+      })
+    }
+  }, [ref])
+  return [setRef, imagesLoaded]
 }
+
+export default useImagesLoaded
